846134fb8c6233ee60d0397ae34817d9e94896fd
carterjaymoore <carsumoore@hotmail.com>
2019-11-05 19:54:30 -0700

    Initial commit.

---
+++ Average.py
@@ -0,0 +1,11 @@
+from statistics import mean
+min=0
+max=101
+print(f'The average of a list from "{min}" to "{max}" is "{mean(range(min,max+1))}"')
+mylist = range(0, 6)
+# averagelist = []
+# for i in range(min,max-10):
+#     meantemp=mean(mylist[i:i+11])
+#     averagelist.append(meantemp)
+#
+# print(averagelist)
