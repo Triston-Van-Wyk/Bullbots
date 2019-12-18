06a4e5b9dac2307c96728d3e6738a6ccd15457f7
Triston Van Wyk <39kingrex@gmail.com>
2019-12-17 17:59:40 -0700

    Update odd or even.py

--- odd or even.py
+++ odd or even.py
@@ -3,7 +3,7 @@ def my_function(number):
 x = my_function(1)
 def is_odd(num):
     return num%2 == 1
-print(is_odd(100))
+print(is_odd(int(input("chose number to see if it is odd:"))))
 def is_even(numb):
     return not is_odd(numb)
-print(is_even(1.4))
+print(is_even(int(input("chose number to see if it is even:"))))
